361e0fd1ace9b8daa53f28e7dde6d22c34750c1f
Felix H W <felixhow@icloud.com>
2020-03-22 18:03:42 +0100

    Increase font size and padding

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Roboto Mono:pixelsize=24:antialias=true:autohint=true";
-static int borderpx = 120;
+static char *font = "Roboto Mono:pixelsize=28:antialias=true:autohint=true";
+static int borderpx = 160;
 
 /*
  * What program is execed by st depends of these precedence rules:
