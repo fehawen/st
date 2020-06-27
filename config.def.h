84530155537f3bfa25d07897a7203ffdae70c1fb
Felix Hägglund Wennergren <felixhow@icloud.com>
2020-06-27 05:02:38 +0200

    st: Apply ligatures patch
    Applies: st-ligatures-scrollback-20200428-28ad288.diff

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Roboto Mono:pixelsize=34:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=34:antialias=true:autohint=true";
 static int borderpx = 150;
 
 /*
