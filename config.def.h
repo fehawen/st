726fa3adbec3c26e80539578831a445f1d97278d
Felix Hägglund Wennergren <felixhow@icloud.com>
2020-07-30 23:26:06 +0200

    config: Change font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Roboto Mono:pixelsize=35:antialias=true:autohint=true";
+static char *font = "SFMono:pixelsize=35:antialias=true:autohint=true";
 static int borderpx = 140;
 
 /*
